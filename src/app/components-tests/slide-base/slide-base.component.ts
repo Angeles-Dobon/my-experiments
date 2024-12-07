6e946c99bf4e151b923d43e5c132f44fe688d9e8
Angeles Dobon <adobon@infoport.es>
2024-12-07 11:51:41 +0100

    3-Un solo método para crear textos e imágenes

--- src/app/components-tests/slide-base/slide-base.component.ts
+++ src/app/components-tests/slide-base/slide-base.component.ts
@@ -17,7 +17,7 @@ export class SlideBaseComponent {
     private dragDrop: DragDrop
   ) {}
 
-  addElement() {
+  addElement(type: string, content: string) {
     // Seleccionar el contenedor
     const container = this.el.nativeElement.querySelector('#example-boundary');
 
@@ -35,56 +35,23 @@ export class SlideBaseComponent {
     this.renderer.addClass(newElement, 'example-box');
 
     // Agregar contenido al nuevo elemento
-    const text = this.renderer.createText(
-      'Este es un nuevo elemento arrastrable'
-    );
-    this.renderer.appendChild(newElement, text);
+    if (type === 'image') {
+      // Crear el elemento de imagen
+      const imgElement = this.renderer.createElement('img');
+
+      // Configurar atributos de la imagen
+      this.renderer.setAttribute(imgElement, 'src', content); // Ruta de la imagen
+      this.renderer.setAttribute(imgElement, 'alt', 'texto-aleternativo'); // Texto alternativo
+      this.renderer.addClass(imgElement, 'example-image'); // Clase para estilos
+
+      this.renderer.appendChild(newElement, imgElement);
+    } else if (type === 'text') {
+      const text = this.renderer.createText(content);
+
+      this.renderer.appendChild(newElement, text);
+    }
 
     // Insertar el elemento en el contenedor
-    this.renderer.appendChild(container, newElement);
-
-    // Crear referencia de drag para el nuevo elemento
-    const dragRef = this.dragDrop.createDrag(newElement);
-
-    // Limitar el movimiento al contenedor
-    dragRef.withBoundaryElement(container);
-
-    this.dragRefs.push(dragRef);
-  }
-
-  addImage() {
-    // Seleccionar el contenedor
-    const container = this.el.nativeElement.querySelector('#example-boundary');
-
-    // Crear un nuevo elemento para contener la imagen
-    const newElement = this.renderer.createElement('div');
-
-    // Agregar clases y atributos para el elemento contenedor
-    this.renderer.setAttribute(
-      newElement,
-      'cdkDragBoundary',
-      '#example-boundary'
-    );
-    this.renderer.setAttribute(newElement, 'cdkDrag', '');
-    this.renderer.addClass(newElement, 'cdk-drag');
-    this.renderer.addClass(newElement, 'example-box');
-
-    // Crear el elemento de imagen
-    const imgElement = this.renderer.createElement('img');
-
-    // Configurar atributos de la imagen
-    this.renderer.setAttribute(
-      imgElement,
-      'src',
-      'assets/images/vector-web-icon.jpg'
-    ); // Ruta de la imagen
-    this.renderer.setAttribute(imgElement, 'alt', 'vector-web-icon'); // Texto alternativo
-    this.renderer.addClass(imgElement, 'example-image'); // Clase para estilos
-
-    // Insertar la imagen dentro del contenedor
-    this.renderer.appendChild(newElement, imgElement);
-
-    // Insertar el nuevo elemento en el contenedor principal
     this.renderer.appendChild(container, newElement);
 
     // Crear referencia de drag para el nuevo elemento
